61e795a9d810f500ff60178f93619ce8b94c156b
aayushhh12 <117062858+aayushhh12@users.noreply.github.com>
2024-01-16 23:34:08 +0530

    Add files via upload

---
+++ 8.4.cpp
@@ -0,0 +1,77 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main(){
+//
+//	for (int i = 0; i < 100; i++)
+//	{
+//		if(i%3==0){
+//			continue;
+//		}
+//		cout<<i <<endl;
+//	}
+//	
+//	return 0;
+//}
+////code to not print Multiple of 3  
+//#include<iostream>
+//using namespace std;
+//
+//int main(){
+//	
+//	int n;
+//	cout<<"Enter a number: ";
+//	cin>>n;
+//	
+//	for(int i=1; i<=n; i++){
+//		if(i%2==0){
+//			continue;
+//		}
+//		cout<<i<<endl;
+//	}
+//	return 0;
+//}
+//#include<iostream>
+//using namespace std;
+
+//int main(){
+//	int n;
+//	cout<<"Enter a number: ";
+//	cin>>n;
+//	
+//	int i;
+//	for (i=2; i<n; i++) {
+//		if (n%i==0){
+//			cout<<"Not a prime"<<endl;
+//			break;
+//		}
+//	}
+//	if(i==n){
+//		cout<<"Prime Number"<<endl;
+//	}
+//	return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//
+//int main(){
+//	
+//	int a,b;
+//	cout<<"Enter 2 number: ";
+//	cin>>a>>b;
+//	
+//	for(int num=a; num<=b; num++){
+//		int i;
+//		for (i=2; i<num; i++){
+//			if(num%i==0){
+//				break;
+//			}
+//		}
+//		if(i==num){
+//			cout<<num<<endl;
+//		}
+//	}
+//	return 0;
+//}
+
+
